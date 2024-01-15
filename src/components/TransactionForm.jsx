29ea6c0cd30e36cb64b74bc49b06acb487b5ce2b
deuryferreira <deury.fr@gmail.com>
2024-01-15 17:24:25 -0400

    Form

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -3,7 +3,7 @@ function TransactionForm() {
     return (
         <form action="">
             <input type="text" placeholder="Enter a Description" />
-            <input type="number" placeholder="Enter a Description" />
+            <input type="number" placeholder="Enter an Amount" />
         </form>
     )
 }
